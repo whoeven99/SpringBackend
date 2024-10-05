0138f82fe6b286b75c78ec61536152d854f0de49
whoeven <>
2024-10-05 19:13:29 +0800

    Test Action

--- src/main/java/com/example/restservice/greeting/GreetingController.java
+++ src/main/java/com/example/restservice/greeting/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "HelloHello, %s!";
+	private static final String template = "HelloHello1111, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
