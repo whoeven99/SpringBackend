bf409fc610ecef54c17ae1d8e4bd4439fd1e15d3
Daoyeee <1287127238@qq.com>
2024-10-24 11:08:53 +0800

    修改添加货币的实现逻辑

---
+++ src/main/java/com/bogdatech/query/TestQuery.java
@@ -0,0 +1,12 @@
+package com.bogdatech.query;
+
+public class TestQuery {
+    public static final String TEST_QUERY = "{\n" +
+            "    shopLocales {\n" +
+            "        name\n" +
+            "        locale\n" +
+            "        primary\n" +
+            "        published\n" +
+            "    }\n" +
+            "}";
+}
