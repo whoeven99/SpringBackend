c482b9df0f3fcd3bb1a225ce51583105bb908d82
Daoyeee <1287127238@qq.com>
2024-10-31 11:30:23 +0800

    1,修改了shopify查询语句，有after时无值或赋值不正确都会报错

--- src/main/java/com/bogdatech/query/TestQuery.java
+++ src/main/java/com/bogdatech/query/TestQuery.java
@@ -6,6 +6,31 @@ public class TestQuery {
 
     public String getTestQuery(TranslateResourceDTO translateResourceDTO){
         String testQuery = "{\n" +
+                "  translatableResources(resourceType: " + translateResourceDTO.getResourceType() + ", first: " + translateResourceDTO.getFirst() + ") {\n" +
+                "    nodes {\n" +
+                "      translations(locale: \""+ translateResourceDTO.getTarget() +"\") {\n" +
+                "        locale\n" +
+                "        value\n" +
+                "      }\n" +
+                "      translatableContent {\n" +
+                "        digest\n" +
+                "        key\n" +
+                "        type\n" +
+                "        locale\n" +
+                "        value\n" +
+                "      }\n" +
+                "    }\n" +
+                "    pageInfo {\n" +
+                "      endCursor\n" +
+                "      hasNextPage\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        return testQuery;
+    }
+
+    public String getAfterQuery(TranslateResourceDTO translateResourceDTO){
+        String testQuery = "{\n" +
                 "  translatableResources(resourceType: " + translateResourceDTO.getResourceType() + ", first: " + translateResourceDTO.getFirst() + ", after: " + translateResourceDTO.getAfter() + ") {\n" +
                 "    nodes {\n" +
                 "      translations(locale: \""+ translateResourceDTO.getTarget() +"\") {\n" +
