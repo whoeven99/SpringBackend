7b0cdb7f4c95a9ae6130f536dec57e4591a624b2
whoeven <>
2024-11-08 01:16:14 +0800

    Update

--- src/main/java/com/bogdatech/Application.java
+++ src/main/java/com/bogdatech/Application.java
@@ -23,7 +23,7 @@ public class Application {
             properties.load(Application.class.getClassLoader().getResourceAsStream("application.properties"));
 
             String env = System.getenv("ApplicationEnv");
-            String connectionUrl = env.equals("prod") ? properties.getProperty("prodSqlUrl") : properties.getProperty("devSqlUrl");
+            String connectionUrl = "prod".equals(env) ? properties.getProperty("prodSqlUrl") : properties.getProperty("devSqlUrl");
             //当 encrypt 属性设置为 true 且 trustServerCertificate 属性设置为 true 时，Microsoft JDBC Driver for SQL Server 将不验证SQL Server TLS 证书。 此设置常用于允许在测试环境中建立连接，如 SQL Server 实例只有自签名证书的情况。
             return DriverManager.getConnection(connectionUrl);
         } catch (Exception e) {
