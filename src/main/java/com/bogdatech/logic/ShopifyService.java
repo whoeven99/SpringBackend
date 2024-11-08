8d0aaff23663c8798e789cb51595cee810f69751
Daoyeee <1287127238@qq.com>
2024-11-08 10:31:27 +0800

    1,实现了获取用户状态的方法 2，产品要求重新翻译一下products和主题，测试翻译质量

--- src/main/java/com/bogdatech/logic/ShopifyService.java
+++ src/main/java/com/bogdatech/logic/ShopifyService.java
@@ -1,11 +1,16 @@
 package com.bogdatech.logic;
 
+import com.alibaba.fastjson.JSONObject;
 import com.bogdatech.entity.TranslateResourceDTO;
 import com.bogdatech.integration.ShopifyHttpIntegration;
 import com.bogdatech.model.controller.request.ShopifyRequest;
+import com.bogdatech.query.ShopifyQuery;
 import com.bogdatech.repository.JdbcRepository;
+import com.bogdatech.utils.CharacterCountUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import static com.bogdatech.entity.TranslateResourceDTO.translationResources;
 
 @Component
 public class ShopifyService {
@@ -16,7 +21,17 @@ public class ShopifyService {
     @Autowired
     private JdbcRepository jdbcRepository;
 
-    public void getTotalWords(String objectData, ShopifyRequest request, TranslateResourceDTO translateResourceDTO){
+    ShopifyQuery shopifyQuery = new ShopifyQuery();
 
+    //获得翻译前一共需要消耗的字符数
+    public void getTotalWords(ShopifyRequest request){
+        CharacterCountUtils counter = new CharacterCountUtils();
+        for (TranslateResourceDTO translateResource : translationResources) {
+            translateResource.setTarget(request.getTarget());
+            String query = shopifyQuery.getFirstQuery(translateResource);
+            //调用逻辑 本地-》 云服务器 -》 云服务器 -》shopify -》本地
+            JSONObject infoByShopify = shopifyApiIntegration.getInfoByShopify(request, query);
+//            countBeforeTranslateChars(infoByShopify, request, translateResource, counter);
+        }
     }
 }
