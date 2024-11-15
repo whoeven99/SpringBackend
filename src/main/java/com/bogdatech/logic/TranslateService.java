69957c1528ed6e3b0219640624b4d4f1d855b9db
Daoyeee <1287127238@qq.com>
2024-11-15 09:00:45 +0800

    1,恢复翻译功能并测试

--- src/main/java/com/bogdatech/logic/TranslateService.java
+++ src/main/java/com/bogdatech/logic/TranslateService.java
@@ -185,7 +185,7 @@ public class TranslateService {
 
     //根据返回的json片段，将符合条件的value翻译,并返回json片段
     public void translateJson(String objectData, ShopifyRequest request, TranslateResourceDTO translateResourceDTO, CharacterCountUtils counter) {
-//        System.out.println("现在翻译到： " + translateResourceDTO.getResourceType());
+         appInsights.trackTrace("现在翻译到： " + translateResourceDTO.getResourceType());
 
         if (objectData == null) {
             throw new IllegalArgumentException("Argument 'content' cannot be null or empty.");
