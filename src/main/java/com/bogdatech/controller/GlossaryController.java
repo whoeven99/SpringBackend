5f61fabe8c4afa864340b5206f54d25033f21f63
Daoyeee <1287127238@qq.com>
2024-12-13 14:33:39 +0800

    1,对词汇表插入和修改进行判定，不允许相同source有相同的rangeCode

--- src/main/java/com/bogdatech/controller/GlossaryController.java
+++ src/main/java/com/bogdatech/controller/GlossaryController.java
@@ -6,43 +6,81 @@ import com.bogdatech.model.controller.response.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.bogdatech.enums.ErrorEnum.*;
 
 @RestController
+@RequestMapping("/glossary")
 public class GlossaryController {
 
     @Autowired
     private IGlossaryService glossaryService;
 
     //插入glossary数据
-    @PostMapping("/glossary/insertGlossaryInfo")
+    @PostMapping("/insertGlossaryInfo")
     public BaseResponse<Object> insertGlossaryInfo(@RequestBody GlossaryDO glossaryDO) {
-        if (glossaryService.insertGlossaryInfo(glossaryDO)){
-            return new BaseResponse<>().CreateSuccessResponse(glossaryService.getSingleGlossaryByShopNameAndSource(glossaryDO.getShopName(), glossaryDO.getSourceText()));
+        //判断 如果数据库中有5个就不再插入了
+        GlossaryDO[] singleGlossaryByShopNameAndSource = glossaryService.getGlossaryByShopName(glossaryDO.getShopName());
+        if (singleGlossaryByShopNameAndSource.length >= 5){
+            return new BaseResponse<>().CreateErrorResponse(SQL_INSERT_ERROR);
         }
-      return new BaseResponse<>().CreateErrorResponse(SQL_INSERT_ERROR);
+        //判断是否冲突（sourceText， rangeCode， caseSensitive）
+        for (GlossaryDO glossary: singleGlossaryByShopNameAndSource) {
+            if (glossary.getSourceText().equals(glossaryDO.getSourceText())) {
+                if (glossary.getRangeCode().equals(glossaryDO.getRangeCode()) || glossary.getRangeCode().equals("ALL") || glossaryDO.getRangeCode().equals("ALL")) {
+                    return new BaseResponse<>().CreateErrorResponse(SQL_INSERT_ERROR);
+                }
+            }
+        }
+
+        try {
+            if (glossaryService.insertGlossaryInfo(glossaryDO)){
+                return new BaseResponse<>().CreateSuccessResponse(glossaryDO);
+            }
+        } catch (Exception e) {
+            return new BaseResponse<>().CreateErrorResponse(SQL_INSERT_ERROR);
+        }
+        return new BaseResponse<>().CreateErrorResponse(SQL_INSERT_ERROR);
     }
 
     //根据id删除glossary数据
-    @PostMapping("/glossary/deleteGlossaryById")
+    @PostMapping("/deleteGlossaryById")
     public BaseResponse<Object> deleteGlossaryById(@RequestBody GlossaryDO glossaryDO) {
         if (glossaryService.deleteGlossaryById(glossaryDO)){
-            return new BaseResponse<>().CreateSuccessResponse(200);
+            return new BaseResponse<>().CreateSuccessResponse(glossaryDO);
         }
         return new BaseResponse<>().CreateErrorResponse(SQL_DELETE_ERROR);
     }
 
     //根据shopName获得glossary数据
-    @PostMapping("/glossary/getGlossaryByShopName")
+    @PostMapping("/getGlossaryByShopName")
     public BaseResponse<Object> getGlossaryByShopName(@RequestBody GlossaryDO glossaryDO) {
         return new BaseResponse<>().CreateSuccessResponse(glossaryService.getGlossaryByShopName(glossaryDO.getShopName()));
     }
 
     //根据id修改targetText，status，rangeCode，caseSensitive数据
-    @PostMapping("/glossary/updateTargetTextById")
+    @PostMapping("/updateTargetTextById")
     public BaseResponse<Object> updateTargetTextById(@RequestBody GlossaryDO glossaryDO) {
+        GlossaryDO[] singleGlossaryByShopNameAndSource = glossaryService.getGlossaryByShopName(glossaryDO.getShopName());
+        //判断是否冲突（sourceText， rangeCode， caseSensitive）
+        for (GlossaryDO glossary: singleGlossaryByShopNameAndSource) {
+            if (glossary.getSourceText().equals(glossaryDO.getSourceText())) {
+                //如果相同就直接返回
+//                System.out.println("glossary.getcode()=" + glossary.getRangeCode());
+//                System.out.println("glossaryDO.getcode()=" + glossaryDO.getRangeCode());
+                //找冲突
+                if (glossary.getRangeCode().equals("ALL") || glossaryDO.getRangeCode().equals("ALL") ) {
+                    return new BaseResponse<>().CreateErrorResponse(SQL_INSERT_ERROR);
+                }
+
+                if (glossary.getRangeCode().equals(glossaryDO.getRangeCode())){
+                    break;
+                }
+
+            }
+        }
         if (glossaryService.updateGlossaryInfoById(glossaryDO)){
             return new BaseResponse<>().CreateSuccessResponse(glossaryDO);
         }
