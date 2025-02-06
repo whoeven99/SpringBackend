4aade02e10140db24b390b4086d453eda4986f6d
Daoyeee <1287127238@qq.com>
2025-02-06 09:57:55 +0800

    1，不禁用静态资源路径

--- src/main/java/com/bogdatech/config/WebConfig.java
+++ src/main/java/com/bogdatech/config/WebConfig.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
     @Bean
     public CorsFilter corsFilter() {
@@ -30,8 +28,4 @@ public class WebConfig implements WebMvcConfigurer {
         return new CorsFilter(corsConfigurationSource);
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // 禁用静态资源路径
-    }
 }
