08ac352a0f137e4da656c4df4ece331867b30adc
Daoyeee <1287127238@qq.com>
2025-03-10 14:02:15 +0800

    1,修改google调用方法

---
+++ src/main/java/com/bogdatech/controller/PrivateKeyController.java
@@ -0,0 +1,10 @@
+package com.bogdatech.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/privateKey")
+public class PrivateKeyController {
+
+}
