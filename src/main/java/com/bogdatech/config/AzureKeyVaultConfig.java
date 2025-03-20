c205e66eefca5be24a49d0e028ba2313ab85d24c
Daoyeee <1287127238@qq.com>
2025-03-20 15:40:35 +0800

    1,修改编码问题 2，修改secret调用方法

--- src/main/java/com/bogdatech/config/AzureKeyVaultConfig.java
+++ src/main/java/com/bogdatech/config/AzureKeyVaultConfig.java
@@ -1,5 +1,7 @@
 package com.bogdatech.config;
 
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.identity.ManagedIdentityCredential;
 import com.azure.identity.ManagedIdentityCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
@@ -11,22 +13,28 @@ import org.springframework.context.annotation.Configuration;
 public class AzureKeyVaultConfig {
     @Bean
     public SecretClient secretClient() {
-        String clientId = "7890ba67-d8e5-4aa1-a67c-9bb4a9a93b91"; //  clientId
-        String keyVaultUrl = "https://springbackendvault.vault.azure.net/"; //  keyVaultUrl
-        ManagedIdentityCredential credential = new ManagedIdentityCredentialBuilder()
-                .clientId(clientId)
-                .build();
+        // 根据环境变量获取clientId和keyVaultUrl来判断选择什么
+        String env = System.getenv("ApplicationEnv");
+        if ("prod".equals(env) || "dev".equals(env)) {
+            String clientId = System.getenv("Client_ID");
+            String keyVaultUrl = System.getenv("UserPrivateKeyVaultUrl");
+            // 如果是本地开发环境，则用DefaultAzureCredential自动检验身份
+            ManagedIdentityCredential credential = new ManagedIdentityCredentialBuilder()
+                    .clientId(clientId)
+                    .build();
 
-        return new SecretClientBuilder()
-                .vaultUrl(keyVaultUrl)
-                .credential(credential)
-                .buildClient();
-        // 使用 DefaultAzureCredential 自动检测身份
-//        DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredentialBuilder().build();
-//        return new SecretClientBuilder()
-//                .vaultUrl("https://springbackendvault.vault.azure.net/")
-//                .credential(defaultAzureCredential)
-//                .buildClient();
+            return new SecretClientBuilder()
+                    .vaultUrl(keyVaultUrl)
+                    .credential(credential)
+                    .buildClient();
+        } else {
+            //使用 DefaultAzureCredential 自动检测身份
+            DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredentialBuilder().build();
+            return new SecretClientBuilder()
+                    .vaultUrl("https://springbackendvault.vault.azure.net/")
+                    .credential(defaultAzureCredential)
+                    .buildClient();
+        }
     }
 
 }
