08e64822155a02968b798ce0896d4e52bc4829c5
Daoyeee <1287127238@qq.com>
2025-04-10 15:53:53 +0800

    1,只修改混元的提示词 改为在user里面公用

--- src/main/java/com/bogdatech/utils/JsoupUtils.java
+++ src/main/java/com/bogdatech/utils/JsoupUtils.java
@@ -250,13 +250,8 @@ public class JsoupUtils {
             return douBaoTranslate(target, resourceType, request.getContent(), counter);
         }
 
-        if (source.equals("fr") || target.equals("es") || source.equals("es")) {
-            return douBaoTranslate(target, resourceType, request.getContent(), counter);
-        }
-        //韩语，泰语， 阿拉伯语都用hunyuan-large翻译
-//        if (target.equals("ko") || target.equals("th") || target.equals("ar")   ) {
-            return hunYuanTranslate(request.getContent(), resourceType, counter, target, "hunyuan-large");
-//        }
+
+        return hunYuanTranslate(request.getContent(), resourceType, counter, target, "hunyuan-large");
 
     }
 
