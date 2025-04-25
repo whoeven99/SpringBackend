504c6e2e8079e8419110f6f3b6221b5fed565015
Daoyeee <1287127238@qq.com>
2025-04-25 09:49:10 +0800

    1,重启后恢复状态为2的翻译任务

--- src/main/java/com/bogdatech/task/TranslateTask.java
+++ src/main/java/com/bogdatech/task/TranslateTask.java
@@ -1,43 +1,27 @@
 package com.bogdatech.task;
 
-import com.bogdatech.entity.TranslatesDO;
-import com.bogdatech.logic.TranslateService;
-import com.bogdatech.Service.impl.TranslatesServiceImpl;
+import com.bogdatech.logic.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import javax.annotation.PostConstruct;
 
 @Component
 @EnableScheduling
 @EnableAsync
 public class TranslateTask {
 
-//    @Autowired
-    private TranslatesServiceImpl translatesServiceImpl;
+    private final TaskService taskService;
 
     @Autowired
-    private TranslateService translateService;
+    public TranslateTask(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
-//    @PostConstruct
-//    @Scheduled(cron = "0 5 * * * ?")
-    public void translate() {
-        //查询Translates表status为0的数据
-        List<TranslatesDO> list = translatesServiceImpl.readTranslateInfo(0);
-        //遍历list，调用translateService.translate()方法
-        for (TranslatesDO request : list) {
-            //调用翻译
-            System.out.println("正在进行翻译");
-            //翻译功能目前注释中
-//            translateService.test(request);
-            //修改status状态
-//            int i = translatesServiceImpl.updateTranslateStatus(request.getShopName(), 2, request.getTarget());
-            //判断是否修改成功
-//            if (i > 0) {
-//                System.out.println("修改成功");
-//            }
-        }
+    @PostConstruct
+    public void translateStatus2WhenSystemRestart() {
+        taskService.translateStatus2WhenSystemRestart();
     }
 }
