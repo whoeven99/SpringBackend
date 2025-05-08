efa40f801b1091982078e98bd37c6abc0befa36b
Daoyeee <1287127238@qq.com>
2025-05-08 14:05:06 +0800

    1,URL也翻译

--- src/main/java/com/bogdatech/logic/TranslateService.java
+++ src/main/java/com/bogdatech/logic/TranslateService.java
@@ -1115,7 +1115,7 @@ public class TranslateService {
             }
 
             //如果包含相对路径则跳过
-            if (type.equals("FILE_REFERENCE") || type.equals("URL") || type.equals("LINK")
+            if (type.equals("FILE_REFERENCE") || type.equals("LINK")
                     || type.equals("LIST_FILE_REFERENCE") || type.equals("LIST_LINK")
                     || type.equals(("LIST_URL"))
                     || "JSON".equals(type)
