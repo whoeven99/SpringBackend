e2df776390e90176d158c39d88015176e3c6a913
Daoyeee <1287127238@qq.com>
2025-05-13 13:13:24 +0800

    1, applicaton的配置，需要修改

--- src/main/java/com/bogdatech/TaskApplication.java
+++ src/main/java/com/bogdatech/TaskApplication.java
@@ -1,16 +1,25 @@
 package com.bogdatech;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableAsync
 public class TaskApplication {
+    @Value("${spring.application.name}")
+    private String name;
     public static void main(String[] args) {
         SpringApplication.run(TaskApplication.class, args);
     }
 
+    @Bean
+    public String name2(){
+        System.out.println("2This is " + name);
+        return name;
+    }
 }
