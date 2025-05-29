fa502c4cc938b5a68ff6f4e6d366d1fc2ffdd444
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2025-05-29 17:32:26 +0800

    1，修改shopify数据的返回，对null的处理 2，10位大写字母和数字不翻译 3，普通提示词加一段

--- src/main/java/com/bogdatech/utils/PlaceholderUtils.java
+++ src/main/java/com/bogdatech/utils/PlaceholderUtils.java
@@ -144,9 +144,9 @@ public class PlaceholderUtils {
      * */
     public static String getSimplePrompt(String target, String languagePackId){
         if (languagePackId != null && !languagePackId.isEmpty()){
-            return "Translate the following text into " + target + " using terminology and tone appropriate for the " + languagePackId + ". Do not output any notes, annotations, explanations, corrections, or bilingual text.Even if you detect an error in the original, do not mention it—only output the final correct translation.";
+            return "Translate the following text into " + target + " using terminology and tone appropriate for the " + languagePackId + ". If it is determined that the text does not need to be translated, please output the original text. Do not output any notes, annotations, explanations, corrections, or bilingual text.Even if you detect an error in the original, do not mention it—only output the final correct translation.";
         }
-        return "Translate the following text into " + target + ". Do not output any notes, annotations, explanations, corrections, or bilingual text.Even if you detect an error in the original, do not mention it—only output the final correct translation.";
+        return "Translate the following text into " + target + ". If it is determined that the text does not need to be translated, please output the original text. Do not output any notes, annotations, explanations, corrections, or bilingual text.Even if you detect an error in the original, do not mention it—only output the final correct translation.";
     }
 
     /**
