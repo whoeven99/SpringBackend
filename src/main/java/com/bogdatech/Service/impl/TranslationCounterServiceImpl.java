d9476cfc4b14d7e4e0b8f77d4dab335040b9d1b0
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2025-07-10 10:56:49 +0800

    1，做重试机制

--- src/main/java/com/bogdatech/Service/impl/TranslationCounterServiceImpl.java
+++ src/main/java/com/bogdatech/Service/impl/TranslationCounterServiceImpl.java
@@ -9,6 +9,8 @@ import com.bogdatech.model.controller.request.TranslationCounterRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.bogdatech.utils.CaseSensitiveUtils.appInsights;
+
 @Service
 @Transactional
 public class TranslationCounterServiceImpl extends ServiceImpl<TranslationCounterMapper, TranslationCounterDO> implements ITranslationCounterService {
@@ -46,8 +48,34 @@ public class TranslationCounterServiceImpl extends ServiceImpl<TranslationCounte
 
     @Override
     public Boolean updateAddUsedCharsByShopName(String shopName, Integer usedChars, Integer maxChars) {
-        //TODO:做重试机制
-        return baseMapper.updateAddUsedCharsByShopName(shopName, usedChars, maxChars);
+        final int maxRetries = 3;
+        final long retryDelayMillis = 500;
+        int retryCount = 0;
+
+        while (retryCount < maxRetries) {
+            try {
+                Boolean b = baseMapper.updateAddUsedCharsByShopName(shopName, usedChars, maxChars);
+                if (Boolean.TRUE.equals(b)) {
+                    return true;
+                } else {
+                    retryCount++;
+                    appInsights.trackTrace("更新失败（返回false） errors ，准备第" + retryCount + "次重试，shopName=" + shopName);
+                }
+            } catch (Exception e) {
+                retryCount++;
+                appInsights.trackTrace("更新失败（抛异常） errors ，准备第" + retryCount + "次重试，shopName=" + shopName + ", 错误=" + e);
+            }
+
+            try {
+                Thread.sleep(retryDelayMillis);
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+                return false;
+            }
+        }
+
+        appInsights.trackTrace("更新失败，重试" + maxRetries + "次后仍未成功，shopName=" + shopName);
+        return false;
     }
 
 
