fcfa43fd57e63f74c83e0e465fa0d14ec62cd718
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2025-07-24 14:38:05 +0800

    1, 生成两个字段，pageType和contentType

--- src/main/java/com/bogdatech/mapper/APGUserProductMapper.java
+++ src/main/java/com/bogdatech/mapper/APGUserProductMapper.java
@@ -7,6 +7,6 @@ import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface APGUserProductMapper extends BaseMapper<APGUserProductDO> {
-    @Update("update APG_User_Product set create_vision = (create_vision + 1), generate_content = #{des}, product_type = #{productType}  where product_id = #{productId} and user_id = #{userId} and is_delete = 0")
-    Boolean updateProductVersion(Long userId, String productId, String des, String productType);
+    @Update("update APG_User_Product set create_vision = (create_vision + 1), generate_content = #{des}, page_type = #{pageType} , content_type = #{contentType}  where product_id = #{productId} and user_id = #{userId} and is_delete = 0")
+    Boolean updateProductVersion(Long userId, String productId, String des, String pageType, String contentType);
 }
