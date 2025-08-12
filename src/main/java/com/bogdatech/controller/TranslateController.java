732ed2b13ab699dec73f1730f023f8e0e995d66d
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2025-08-12 16:20:55 +0800

    1, 修改getTranslateDOByShopNameAndSource方法，返回所有的状态为2的数据

--- src/main/java/com/bogdatech/controller/TranslateController.java
+++ src/main/java/com/bogdatech/controller/TranslateController.java
@@ -131,8 +131,9 @@ public class TranslateController {
     @PostMapping("/getTranslateDOByShopNameAndSource")
     public BaseResponse<Object> getTranslateDOByShopNameAndSource(@RequestBody TranslateRequest request) {
         if (request != null) {
-            TranslatesDO translatesDO = translatesService.selectLatestOne(request);
-            return new BaseResponse<>().CreateSuccessResponse(translatesDO);
+            //改为返回状态为2的所有相关数据
+            List<TranslatesDO> list = translatesService.list(new LambdaQueryWrapper<TranslatesDO>().eq(TranslatesDO::getShopName, request.getShopName()).eq(TranslatesDO::getSource, request.getSource()).eq(TranslatesDO::getStatus, 2));
+            return new BaseResponse<>().CreateSuccessResponse(list);
         }
         return new BaseResponse<>().CreateErrorResponse(DATA_IS_EMPTY);
     }
