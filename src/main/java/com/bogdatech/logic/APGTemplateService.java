67fdac28c8ba6c7c170b47cf2aead851531c5635
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2025-11-13 10:21:58 +0800

    1, 修改apg的获取数据，获取前5条数据

--- src/main/java/com/bogdatech/logic/APGTemplateService.java
+++ src/main/java/com/bogdatech/logic/APGTemplateService.java
@@ -13,6 +13,8 @@ import com.bogdatech.entity.DTO.TemplateDTO;
 import com.bogdatech.utils.TypeConversionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
