68fd09b6e098556b9168dfd8694a7673c39f43fd
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2025-11-13 15:02:51 +0800

    1, 修改返回值

--- src/main/java/com/bogdatech/controller/PCPictureEditController.java
+++ src/main/java/com/bogdatech/controller/PCPictureEditController.java
@@ -2,19 +2,17 @@ package com.bogdatech.controller;
 
 import com.bogdatech.integration.AidgeIntegration;
 import com.bogdatech.model.controller.request.SignRequest;
-import com.bogdatech.model.controller.response.BaseResponse;
+import com.bogdatech.model.controller.response.AidgeResponse;
 import com.bogdatech.model.controller.response.SignResponse;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/pc/edit")
 public class PCPictureEditController {
 
     @PostMapping("/getSignResponse")
-    public BaseResponse<Object> getSignResponse(@RequestBody SignRequest signRequest) {
+    public AidgeResponse<Object> getSignResponse(@RequestBody SignRequest signRequest) {
         SignResponse signResponse = AidgeIntegration.getSignResponse(signRequest.getParams(), signRequest.getApi());
-        return new BaseResponse<>().CreateSuccessResponse(signResponse);
+        return new AidgeResponse<>().CreateSuccessResponse(signResponse);
     }
 }
