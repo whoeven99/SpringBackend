7b7ab11e741c287d5f6566b14f276ff296501b40
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2025-12-11 11:55:53 +0800

    1 在PC_User_Pictures表中添加一个mediaId字段，用于前端存储对应数据

--- src/main/java/com/bogdatech/logic/PCApp/PCUserPicturesService.java
+++ src/main/java/com/bogdatech/logic/PCApp/PCUserPicturesService.java
@@ -45,8 +45,7 @@ public class PCUserPicturesService {
 
     public static String CDN_URL = "https://img.bogdatech.com";
     public static String COS_URL = "https://ciwi-us-1327177217.cos.na-ashburn.myqcloud.com";
-    public static int APP_PIC_FEE = 2000;
-    public static int APP_ALT_FEE = 2000; // alt和pic翻译一块扣除
+    public static int APP_PIC_FEE = 2000;  // alt和pic翻译一块扣除
 
     public BaseResponse<Object> insertPicToDbAndCloud(MultipartFile file, String shopName, String pcUserPicturesDoJson) {
         //解析userPicturesDO
