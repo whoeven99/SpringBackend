3abeab40b7a75bfe8e5e54bbfc4b1355734fd0d8
whoeven99 <whoeven99@outlook.com>
2025-12-23 17:07:00 +0800

    添加 task module

---
+++ BogdaTask/src/main/java/com/bogda/task/task/TestTask.java
@@ -0,0 +1,18 @@
+package com.bogda.task.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestTask {
+
+    private static final Logger log = LoggerFactory.getLogger(TestTask.class);
+
+    // 每5秒执行一次
+    @Scheduled(fixedRate = 5000)
+    public void testJob() {
+        log.info("TestTask 执行了一次定时任务");
+    }
+}
