0754e1b6ed285726a2337ae1d34eea532851fb35
whoeven99 <whoeven99@outlook.com>
2025-12-23 16:01:05 +0800

    delete

--- src/main/java/com/bogdatech/Service/ITranslationUsageService.java
+++
@@ -1,9 +0,0 @@
-package com.bogdatech.Service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.bogdatech.entity.DO.TranslationUsageDO;
-
-import java.util.List;
-
-public interface ITranslationUsageService extends IService<TranslationUsageDO> {
-}
