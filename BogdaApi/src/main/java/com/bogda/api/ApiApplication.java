f1b34200af5ac24f89b81ece7872301978762949
whoeven99 <whoeven99@outlook.com>
2025-12-23 16:26:31 +0800

    Update

--- BogdaApi/src/main/java/com/bogda/api/ApiApplication.java
+++ BogdaApi/src/main/java/com/bogda/api/ApiApplication.java
@@ -2,10 +2,12 @@ package com.bogda.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableAsync
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
