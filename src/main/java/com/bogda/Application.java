a1ca9a65e7de51926f09e454780e9a2dc3dda08d
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2026-01-05 09:46:35 +0800

    1, 添加application

---
+++ src/main/java/com/bogda/Application.java
@@ -0,0 +1,15 @@
+package com.bogda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableScheduling
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
