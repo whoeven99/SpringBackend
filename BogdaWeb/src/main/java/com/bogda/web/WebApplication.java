75251c7fdf9d8b34bdff4e38102863ab305ede32
whoeven99 <whoeven99@outlook.com>
2026-01-13 20:02:06 +0800

    Add BogdaWeb for api

--- BogdaWeb/src/main/java/com/bogda/web/WebApplication.java
+++ BogdaWeb/src/main/java/com/bogda/web/WebApplication.java
@@ -1,4 +1,4 @@
-package com.bogda.api;
+package com.bogda.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +8,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(
     exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class},
-    scanBasePackages = {"com.bogda.api", "com.bogda.repository", "com.bogda.common", "com.bogda.integration"}
+    scanBasePackages = {"com.bogda.web", "com.bogda.api", "com.bogda.repository", "com.bogda.common", "com.bogda.integration"}
 )
 @EnableAsync
-public class ApiApplication {
+public class WebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication.run(WebApplication.class, args);
     }
 }
+
