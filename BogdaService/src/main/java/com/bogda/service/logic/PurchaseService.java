76592ee887671fc7c12822c34d97533fa7d6744f
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2026-01-20 14:42:47 +0800

    1, 修改rate存储，改为redis存储

--- BogdaService/src/main/java/com/bogda/service/logic/PurchaseService.java
+++ BogdaService/src/main/java/com/bogda/service/logic/PurchaseService.java
@@ -7,14 +7,15 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-import static com.bogda.integration.aimodel.RateHttpIntegration.rateMap;
-import static com.bogda.service.logic.RateDataService.getRateByRateMap;
 
 @Component
 public class PurchaseService {
 
     @Autowired
     private ICurrenciesService currenciesService;
+
+    @Autowired
+    private RateDataService rateDataService;
 
     //从缓存中获取数据，根据传入的数据作为判断条件
     public Map<String, Object> getCacheData(CurrenciesDO currencyDO){
@@ -26,14 +27,14 @@ public class PurchaseService {
         String defaultCurrencyCode = currenciesService.getCurrencyCodeByPrimaryStatusAndShopName(currencyDO.getShopName());
         //当exchangeRate为Auto时，从缓存中获取对应货币代码数据数据
         if (currencyWithSymbol.get("primaryStatus").equals(0) && "Auto".equals(currencyWithSymbol.get("exchangeRate"))) {
-            if (rateMap.isEmpty()){
+            if (rateDataService.isRateCacheEmpty()){
                 return currencyWithSymbol;
             }
             //与默认货币代码的汇率
             if (defaultCurrencyCode.isEmpty()){
                 return currencyWithSymbol;
             }
-            double rateByRateMap = getRateByRateMap(defaultCurrencyCode, currencyDO.getCurrencyCode());
+            double rateByRateMap = rateDataService.getRateByRateMap(defaultCurrencyCode, currencyDO.getCurrencyCode());
             currencyWithSymbol.put("exchangeRate", rateByRateMap);
         }
         return currencyWithSymbol;
