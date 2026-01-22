388619ae5fecdef9918926ccb880f881c59d39f6
whoeven99 <whoeven99@outlook.com>
2026-01-22 12:50:25 +0800

    update

--- BogdaIntegration/src/main/java/com/bogda/integration/Config.java
+++ BogdaIntegration/src/main/java/com/bogda/integration/Config.java
@@ -4,12 +4,13 @@ import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.google.genai.Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AzureKeyVaultConfig {
+public class Config {
     @Value("${keyvault.endpoint}")
     private String url;
 
@@ -22,17 +23,27 @@ public class AzureKeyVaultConfig {
     @Value("${appRegistration.tenant-id}")
     private String tenantId;
 
-   @Bean
-   public SecretClient secretClient() {
-       ClientSecretCredential credential = new ClientSecretCredentialBuilder()
-               .clientId(clientId)
-               .clientSecret(clientSecret)
-               .tenantId(tenantId)
-               .build();
+    @Bean
+    public SecretClient secretClient() {
+        ClientSecretCredential credential = new ClientSecretCredentialBuilder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .tenantId(tenantId)
+                .build();
 
-       return new SecretClientBuilder()
-               .vaultUrl(url)
-               .credential(credential)
-               .buildClient();
-   }
+        return new SecretClientBuilder()
+                .vaultUrl(url)
+                .credential(credential)
+                .buildClient();
+    }
+
+    @Value("${gemini.api.key.vault}")
+    private String key;
+
+    @Bean
+    public Client geminiClient() {
+        return Client.builder()
+                .apiKey(key)
+                .build();
+    }
 }
--- BogdaRepository/src/main/java/com/bogda/repository/config/RedisConfig.java
+++ BogdaRepository/src/main/java/com/bogda/repository/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.bogda.repository;
+package com.bogda.repository.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
