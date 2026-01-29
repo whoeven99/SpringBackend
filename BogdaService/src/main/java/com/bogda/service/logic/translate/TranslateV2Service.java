3a481a4d401db2e09a6dab58e81ed6807e3cadc9
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2026-01-29 17:14:05 +0800

    1, 当主题和元字段的value以=开头时，不翻译

--- BogdaService/src/main/java/com/bogda/service/logic/translate/TranslateV2Service.java
+++ BogdaService/src/main/java/com/bogda/service/logic/translate/TranslateV2Service.java
@@ -1077,12 +1077,17 @@ public class TranslateV2Service {
                     JudgeTranslateUtils.SUSPICIOUS2_PATTERN.matcher(value).matches()) {
                 return false;
             }
+
             if (!JudgeTranslateUtils.metaTranslate(value)) {
                 return false;
             }
 
             // 如果是base64编码的数据，不翻译
             if (JudgeTranslateUtils.BASE64_PATTERN.matcher(value).matches()) {
+                return false;
+            }
+
+            if (value.startsWith("=")){
                 return false;
             }
 
