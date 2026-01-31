5bf8736bbfe49dabde426d05fec25acf63d79d9d
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2026-01-31 17:05:30 +0800

    1, 修改测试用例

--- BogdaTask/src/main/java/com/bogda/task/task/TranslateTask.java
+++ BogdaTask/src/main/java/com/bogda/task/task/TranslateTask.java
@@ -84,6 +84,7 @@ public class TranslateTask {
         }
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 30 * 1000)
     public void initialToTranslateTask() {
         process(0,
@@ -92,6 +93,7 @@ public class TranslateTask {
                 translateV2Service::initialToTranslateTask);
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 30 * 1000)
     public void translateEachTask() {
         process(1,
@@ -100,6 +102,7 @@ public class TranslateTask {
                 translateV2Service::translateEachTask);
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 30 * 1000)
     public void saveToShopify() {
 
@@ -115,11 +118,13 @@ public class TranslateTask {
 
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 300 * 1000)
     public void deleteToShopify() {
         translateV2Service.deleteToShopify();
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 30 * 1000)
     public void sendEmail() {
         // 自动翻译的邮件
@@ -200,6 +205,7 @@ public class TranslateTask {
         }
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 13 * 1000 * 60)
     public void cleanTask() {
         // 3天前 且 isDeleted 的任务清理掉
