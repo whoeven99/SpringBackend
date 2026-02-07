32dccc03b3af973c5294dd71efba8400d9527533
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2026-02-07 09:08:58 +0800

    1, 将翻译task的 和 ip相关的日志迁移到sls

---
+++ BogdaTask/src/main/java/com/bogda/task/config/ExceptionReporterConfig.java
@@ -0,0 +1,25 @@
+package com.bogda.task.config;
+
+import com.bogda.common.reporter.ExceptionReporterHolder;
+import com.bogda.common.reporter.TraceReporterHolder;
+import com.bogda.integration.aimodel.AliyunSlsIntegration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 启动时将 SLS 的 logException / logTrace 注册为 Common 层异常与 Trace 上报实现。
+ */
+@Component
+public class ExceptionReporterConfig {
+
+    @Autowired
+    private AliyunSlsIntegration aliyunSlsIntegration;
+
+    @PostConstruct
+    public void init() {
+        ExceptionReporterHolder.setReporter(aliyunSlsIntegration::logException);
+        TraceReporterHolder.setReporter(aliyunSlsIntegration::logTrace);
+    }
+}
