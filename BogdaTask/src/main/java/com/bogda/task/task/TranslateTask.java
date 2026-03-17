b3e09161b8111ee70d21d7d7b58b243715a3cad2
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2026-03-17 14:06:30 +0800

    1, 打开测试环境的翻译流程

--- BogdaTask/src/main/java/com/bogda/task/task/TranslateTask.java
+++ BogdaTask/src/main/java/com/bogda/task/task/TranslateTask.java
@@ -9,6 +9,7 @@ import com.bogda.service.logic.TencentEmailService;
 import com.bogda.service.logic.translate.TranslateV2Service;
 import com.bogda.repository.entity.InitialTaskV2DO;
 import com.bogda.repository.repo.InitialTaskV2Repo;
+import com.bogda.task.annotation.EnableScheduledTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -86,6 +87,7 @@ public class TranslateTask {
         }
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 30 * 1000)
     public void initialToTranslateTask() {
         process(0,
@@ -94,6 +96,7 @@ public class TranslateTask {
                 translateV2Service::initialToTranslateTask);
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 30 * 1000)
     public void translateEachTask() {
         process(1,
@@ -102,6 +105,7 @@ public class TranslateTask {
                 translateV2Service::translateEachTask);
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 30 * 1000)
     public void saveToShopify() {
 
@@ -117,11 +121,13 @@ public class TranslateTask {
 
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 300 * 1000)
     public void deleteToShopify() {
         translateV2Service.deleteToShopify();
     }
 
+    @EnableScheduledTask
     @Scheduled(fixedDelay = 30 * 1000)
     public void sendEmail() {
         // 自动翻译的邮件
@@ -203,6 +209,7 @@ public class TranslateTask {
     }
 
     @Scheduled(fixedDelay = 13 * 1000 * 60)
+    @EnableScheduledTask
     public void cleanTask() {
         // 3天前 且 isDeleted 的任务清理掉
         List<InitialTaskV2DO> cleanTask = initialTaskV2Repo.selectTaskBeforeDaysAndDeleted(3);
