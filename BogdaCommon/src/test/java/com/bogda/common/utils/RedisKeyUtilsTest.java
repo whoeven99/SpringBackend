5b45cef9fd3f85dca3e0ccfeb71c5b7c89c1c864
whoeven99 <whoeven99@outlook.com>
2026-07-07 16:11:58 +0800

    清理代码，redis无用代码

--- BogdaCommon/src/test/java/com/bogda/common/utils/RedisKeyUtilsTest.java
+++ BogdaCommon/src/test/java/com/bogda/common/utils/RedisKeyUtilsTest.java
@@ -17,43 +17,8 @@ public class RedisKeyUtilsTest {
     }
 
     @Test
-    void testDay15Constant() {
-        assertEquals(2592000L, RedisKeyUtils.DAY_15);
-    }
-
-    @Test
     void testDay1Constant() {
         assertEquals(86400L, RedisKeyUtils.DAY_1);
-    }
-
-    @Test
-    void testDataReportKeyTemplate() {
-        assertNotNull(RedisKeyUtils.DATA_REPORT_KEY_TEMPLATE);
-        assertEquals("dr:{shopName}:{language}:{yyyyMMdd}", RedisKeyUtils.DATA_REPORT_KEY_TEMPLATE);
-    }
-
-    @Test
-    void testDataReportKeyTemplateKeys() {
-        assertNotNull(RedisKeyUtils.DATA_REPORT_KEY_TEMPLATE_KEYS);
-        assertEquals("drs:{shopName}:keys", RedisKeyUtils.DATA_REPORT_KEY_TEMPLATE_KEYS);
-    }
-
-    @Test
-    void testClientIdSet() {
-        assertNotNull(RedisKeyUtils.CLIENT_ID_SET);
-        assertEquals("ci:{shopName}:{language}:{yyyyMMdd}:{eventName}", RedisKeyUtils.CLIENT_ID_SET);
-    }
-
-    @Test
-    void testStoppedFlag() {
-        assertNotNull(RedisKeyUtils.STOPPED_FLAG);
-        assertEquals("tsk:stp:{shopName}", RedisKeyUtils.STOPPED_FLAG);
-    }
-
-    @Test
-    void testStoppedFlagSingle() {
-        assertNotNull(RedisKeyUtils.STOPPED_FLAG_SINGLE);
-        assertEquals("tsk:stp:{shopName}:{InitialId}", RedisKeyUtils.STOPPED_FLAG_SINGLE);
     }
 }
 
